f8711e99f2bf43389a25ca7e7b4e60e507bb452c
Eduardo-Angeles <eduardo.jes.angeles.hdz@gmail.com>
2024-11-15 15:48:35 -0600

    fase 1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,24 @@
+// tailwind.config.js o tailwind.config.mjs
 import animations from "tailwindcss-animated";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		animations,
-		({ addComponents }) => {
-		  addComponents({
-			".cp-v": {
-			  clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
-			},
-		});
-	},
-],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#6EC1E4',
+        customPink: '#FF69B4', // Añade el color rosa personalizado
+      },
+    },
+  },
+  plugins: [
+    animations,
+    ({ addComponents }) => {
+      addComponents({
+        ".cp-v": {
+			clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
+        },
+      });
+    },
+  ],
 }
