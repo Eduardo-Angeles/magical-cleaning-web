2f1c01921d2db78389424440fb51609f27c7c5f5
Eduardo-Angeles <eduardo.jes.angeles.hdz@gmail.com>
2024-11-20 10:58:14 -0600

    Pagina de galeria, colores, modo dark

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,35 @@
-// tailwind.config.js o tailwind.config.mjs
 import animations from "tailwindcss-animated";
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        customBlue: '#6EC1E4',
-        customPink: '#FF69B4', // Añade el color rosa personalizado
-      },
-    },
-  },
-  plugins: [
-    animations,
-    ({ addComponents }) => {
-      addComponents({
-        ".cp-v": {
-			clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
-        },
-      });
-    },
-  ],
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#1d4ed8",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+        specialBlue: "#172554",
+		specialRose: "#bf195e",
+			},
+			screens:{
+				midmd:"880px"
+			}
+		},
+	},
+	plugins: [
+		animations,
+		({ addComponents }) => {
+		  addComponents({
+			".cp-v": {
+			  clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
+			},
+		});
+	},
+],
 }
