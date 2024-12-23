06fe347ecf87bf7fcea12afdd6fa8a366d74662d
Eduardo-Angeles <eduardo.jes.angeles.hdz@gmail.com>
2024-12-23 08:38:18 -0600

    añadido URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://magical-cleaningservice.com/',
   integrations: [tailwind(), preact(), sitemap()]
 });
